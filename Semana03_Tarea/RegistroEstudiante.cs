8fd0a97614c2aa7924bde8ea849fd78fe9c5e913
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-13 00:00:40 -0500

    agregarmos metodo para  MostrarDatos y uso de  for para imprimir teléfonos

--- Semana03_Tarea/RegistroEstudiante.cs
+++ Semana03_Tarea/RegistroEstudiante.cs
@@ -9,3 +9,17 @@ class RegistroEstudiante
     public string direccion = "Daule Marianita 2";
     // Array para guardar los numeros de telefono 
     public string[] numerostelefonos = { "0963115004", "0963748436", "0963555555" };
+   // Mostramos los datos del estudiante
+    public void MostrarDatos()
+    {
+        Console.WriteLine("ID" + id);
+        Console.WriteLine("Nombres" + nombres);
+        Console.WriteLine("Apellidos" + apellidos);
+        Console.WriteLine("Direccion" + direccion);
+        Console.WriteLine("Telefonos:");
+        for (int i = 0; i < numerostelefonos.Length; i++) //Usamos for para recorrer todos los elementos del array uno por uno.
+        {
+            Console.WriteLine("-" + numerostelefonos[i]);// Muestra en pantalla el numero de Telefono
+        }
+    }
+}
