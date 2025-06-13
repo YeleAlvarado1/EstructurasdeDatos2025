61b36231b122c74ee9fa17190e93809dd7eb2176
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-12 23:51:03 -0500

    Creacion de clase y varibles con valores.

--- Semana03_Tarea/RegistroEstudiante.cs
+++ Semana03_Tarea/RegistroEstudiante.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System;
+// Creacion de clase Resgistro de Estudiante
+class RegistroEstudiante
+{
+    // Declaracion de variables y sus valores
+    public int id = 0951824424; 
+    public string nombres = "Sofia Victoria";
+    public string apellidos = "Alvarado Orellana";
+    public string direccion = "Daule Marianita 2";
