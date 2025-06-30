83aa365e38af8ab106f9e57b6358252018f7e528
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 20:58:56 -0500

    Creacion de carpeta Y ejercicio 1

---
+++ Ejercicios5/Ejercicio1.cs
@@ -0,0 +1,18 @@
+﻿class Programa {
+    static void Main() {
+        System.Console.WriteLine("Ejercicio # 1");
+        System.Console.WriteLine("================");
+
+        System.Collections.Generic.List<string> asignaturas = new System.Collections.Generic.List<string>();
+        asignaturas.Add("Matemáticas");
+        asignaturas.Add("Física");
+        asignaturas.Add("Química");
+        asignaturas.Add("Historia");
+        asignaturas.Add("Lengua");
+        asignaturas.Sort(); //Orden Alfabetico
+        foreach (string asignatura in asignaturas)
+        {
+            System.Console.WriteLine(asignatura);
+        }
+    }
+}
