f9a4ef4c0d4957c6bb34590a72f478a7a0bcfd3e
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 22:45:43 -0500

    Entrada de datos con ReadLine()

--- Semana05_Ejercicio3/Ejercicio3.cs
+++ Semana05_Ejercicio3/Ejercicio3.cs
@@ -1,17 +1,24 @@
-﻿class Programa
-{
+﻿class Programa {
     static void Main()
     {
         System.Collections.Generic.List<string> asignaturas = new System.Collections.Generic.List<string>();
         System.Collections.Generic.List<string> notas = new System.Collections.Generic.List<string>();
+
         asignaturas.Add("Matemáticas");
         asignaturas.Add("Física");
         asignaturas.Add("Química");
         asignaturas.Add("Historia");
         asignaturas.Add("Lengua");
 
+        foreach (string asignatura in asignaturas)
+        {
+            System.Console.Write("¿Qué nota obtuviste en " + asignatura + "?: ");
+            string nota = System.Console.ReadLine() ?? "";
+            notas.Add(nota);
+        }
+
+        }
     }
 }
     
-        
     
