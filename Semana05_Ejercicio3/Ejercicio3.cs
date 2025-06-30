99f70a8509bec01348d944ed16726f496e5b3f1b
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 22:34:06 -0500

    Declaración de listas

--- Semana05_Ejercicio3/Ejercicio3.cs
+++ Semana05_Ejercicio3/Ejercicio3.cs
@@ -2,7 +2,8 @@
 {
     static void Main()
     {
-        
+        System.Collections.Generic.List<string> asignaturas = new System.Collections.Generic.List<string>();
+        System.Collections.Generic.List<string> notas = new System.Collections.Generic.List<string>();
     }
 }
     
