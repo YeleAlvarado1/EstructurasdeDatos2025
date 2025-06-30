872f9b46913a79e5472b0be22568f6c2ae3fc94f
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 23:01:05 -0500

    Conversión de texto a número y prevencion errores se uso ?? "0"

--- Semana05_Ejercicio4/Ejercicio4.cs
+++ Semana05_Ejercicio4/Ejercicio4.cs
@@ -5,6 +5,12 @@
         System.Collections.Generic.List<int> numeros = new System.Collections.Generic.List<int>();
 
         System.Console.WriteLine("Ingrese los 6 números ganadores de la lotería:");
+        for (int i = 1; i <= 6; i++)
+        {
+            System.Console.Write("Número " + i + ": ");
+            string entrada = System.Console.ReadLine() ?? "0";
+            int numero = System.Int32.Parse(entrada);
+            numeros.Add(numero);
+        }
     }
-}
-    
+}    
