a0036d4db55d921ee3cb6a44333819fd316f98e9
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 23:17:37 -0500

    Inicialización de la lista y uso del for

--- Semana05_Ejercicio5/Ejercicio5.cs
+++ Semana05_Ejercicio5/Ejercicio5.cs
@@ -2,7 +2,14 @@
 {
     static void Main()
     {
-        
+        System.Collections.Generic.List<int> numeros = new System.Collections.Generic.List<int>();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            numeros.Add(i);
+        }
     }
+        
 }
+
     
