8b5912cd76d3c13db6f0b5aa84d879f406eef9b0
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 22:34:58 -0500

    Carga manual de asignaturas

--- Semana05_Ejercicio3/Ejercicio3.cs
+++ Semana05_Ejercicio3/Ejercicio3.cs
@@ -4,6 +4,12 @@
     {
         System.Collections.Generic.List<string> asignaturas = new System.Collections.Generic.List<string>();
         System.Collections.Generic.List<string> notas = new System.Collections.Generic.List<string>();
+        asignaturas.Add("Matemáticas");
+        asignaturas.Add("Física");
+        asignaturas.Add("Química");
+        asignaturas.Add("Historia");
+        asignaturas.Add("Lengua");
+
     }
 }
     
