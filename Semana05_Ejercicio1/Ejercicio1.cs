14a9084f075f7a75eff609cb5141a866fc187fc3
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 21:57:09 -0500

    lista genérica List<string> y de sort

--- Semana05_Ejercicio1/Ejercicio1.cs
+++ Semana05_Ejercicio1/Ejercicio1.cs
@@ -6,4 +6,10 @@
         System.Console.WriteLine("================");
 
         System.Collections.Generic.List<string> asignaturas = new System.Collections.Generic.List<string>();
-        
+        asignaturas.Add("Matemáticas");
+        asignaturas.Add("Física");
+        asignaturas.Add("Química");
+        asignaturas.Add("Historia");
+        asignaturas.Add("Lengua");
+        asignaturas.Sort(); //Orden Alfabetico
+     
