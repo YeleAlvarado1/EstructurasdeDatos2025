64ac4a1c4d3d6f0d8104993db4af307fc0ca77c5
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-06-29 22:58:50 -0500

    Declaración de la lista y Solicitud de datos al usuario

--- Semana05_Ejercicio4/Ejercicio4.cs
+++ Semana05_Ejercicio4/Ejercicio4.cs
@@ -2,7 +2,9 @@
 {
     static void Main()
     {
-        
+        System.Collections.Generic.List<int> numeros = new System.Collections.Generic.List<int>();
+
+        System.Console.WriteLine("Ingrese los 6 números ganadores de la lotería:");
     }
 }
     
