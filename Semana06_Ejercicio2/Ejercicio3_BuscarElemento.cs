c169a3d563f7252582adf10f34ddcbaab9d78f13
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-07-05 01:41:20 -0500

    Cambio de nombre de archivo , Inicializar proyecto y crear clases base Nodo y ListaSimple

---
+++ Semana06_Ejercicio2/Ejercicio3_BuscarElemento.cs
@@ -0,0 +1,56 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear una lista simple
+        ListaSimple lista = new ListaSimple();
+
+        // Insertar varios valores algunos repetidos
+        lista.InsertarFinal(5);
+        lista.InsertarFinal(10);
+        lista.InsertarFinal(5);
+        lista.InsertarFinal(20);
+        lista.InsertarFinal(5);
+
+        // Mostrar la lista actual
+        System.Console.WriteLine("Lista actual:");
+        lista.Dibujar();
+
+        // Buscar cuántas veces aparece el número 5
+        int valorBuscado = 5;
+        int veces = lista.BuscarCantidad(valorBuscado);
+
+        // Mostrar el resultado
+        if (veces > 0)
+        {
+            System.Console.WriteLine("El dato " + valorBuscado + " se encontró " + veces + " veces.");
+        }
+        else
+        {
+            System.Console.WriteLine("El dato " + valorBuscado + " no fue encontrado en la lista.");
+        }
+    }
+}
+
+// Clase Nodo 
+public class Nodo
+{
+    public int Dato;            // Dato almacenado en el nodo
+    public Nodo? Siguiente;     
+
+    // Constructor para asignar el valor al nodo
+    public Nodo(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+// Clase ListaSimple (estructura principal)
+public class ListaSimple
+{
+    Nodo? inicio;   // Nodo que representa el inicio de la lista
+
+  
+}
