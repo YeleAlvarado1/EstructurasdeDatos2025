2d1f19e4368a47b01e3551b0df0cd0a36d8b973d
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-07-05 01:04:50 -0500

    Crear estructura Nodo y ListaSimple con métodos básicos InsertarInicio, InsertarFinal

--- Semana06_Ejercicio1/Ejercicio1_ContarElementos.cs
+++ Semana06_Ejercicio1/Ejercicio1_ContarElementos.cs
@@ -9,6 +9,57 @@
         lista.InsertarInicio(5);
         lista.InsertarFinal(10);
         lista.InsertarFinal(15);
+
+        System.Console.WriteLine("Lista actual:");
+        lista.Dibujar();
+
+        int cantidad = lista.Contar();
+        System.Console.WriteLine("Total de nodos: " + cantidad);
+    }
+}
+public class Nodo
+{
+    public int Dato;
+    public Nodo? Siguiente;
+
+    public Nodo(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+public class ListaSimple
+{
+    Nodo? inicio;
+
+    public ListaSimple()
+    {
+        inicio = null;
+    }
+
+    public void InsertarInicio(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+        nuevo.Siguiente = inicio;
+        inicio = nuevo;
+    }
+
+    public void InsertarFinal(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+        if (inicio == null)
+        {
+            inicio = nuevo;
+        }
+        else
+        {
+            Nodo actual = inicio;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevo;
+        }
     }
 }
     
