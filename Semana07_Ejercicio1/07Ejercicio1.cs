4d8566ad38903ea41eae3dd6a2cce1cd004377d7
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-07-12 23:10:37 -0500

    Se uso push para apilar los simbolos y peek mas pop para verificar simbolos

--- Semana07_Ejercicio1/07Ejercicio1.cs
+++ Semana07_Ejercicio1/07Ejercicio1.cs
@@ -1,10 +1,35 @@
 ﻿//Definciion de String de Prueba
+using System.Diagnostics;
+
 string expresion = "[2 * (4 + 1)] + {3 + [(7 - 2) * 5]}";
 //Declaracion de pila 
 Stack<char> simbolos = new();
 // Usamos foreach para recorrer la cadena
 foreach (char c in expresion)
 {
-    
+    if (c == '(' || c == '[' || c == '{')
+    {
+        simbolos.Push(c);
+    }
+    else if (c == ')' || c == ']' || c == '}')
+    {
+        if (simbolos.Count == 0)
+        {
+            Console.WriteLine("Expresión Desbalanceada");
+            return;
+        }
+        char ultimo = simbolos.Peek();
+        if ((c == ')' && ultimo == '(') ||
+            (c == ']' && ultimo == '[') ||
+            (c == '}' && ultimo == '{'))
+
+        {
+            simbolos.Pop();
+        }
+        else
+        {
+            Console.WriteLine("EXPRESIÓN DESBALANCEADA");    
+        }
+    }
 }
 
