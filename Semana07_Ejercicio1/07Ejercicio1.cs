7f03890360c60698bc9f8764f635d424b9daf253
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-07-12 22:53:52 -0500

    Se declaro pila y se uso foreach para rrecorre la cadena

--- Semana07_Ejercicio1/07Ejercicio1.cs
+++ Semana07_Ejercicio1/07Ejercicio1.cs
@@ -1,3 +1,10 @@
-﻿
+﻿//Definciion de String de Prueba
 string expresion = "[2 * (4 + 1)] + {3 + [(7 - 2) * 5]}";
+//Declaracion de pila 
+Stack<char> simbolos = new();
+// Usamos foreach para recorrer la cadena
+foreach (char c in expresion)
+{
+    
+}
 
