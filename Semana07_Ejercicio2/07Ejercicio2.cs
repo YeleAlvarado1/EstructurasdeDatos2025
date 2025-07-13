6381d9f2141e0f6b6e37b4edce500ec783eb304c
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-07-13 00:18:12 -0500

    Se creó el método MoverDisco()

--- Semana07_Ejercicio2/07Ejercicio2.cs
+++ Semana07_Ejercicio2/07Ejercicio2.cs
@@ -9,10 +9,25 @@ for (int i = discos; i >= 1; i--)
 {
     torreA.Push(i);
 }
-// Creacion de metodo mopverhanoi para para resolver el juego mediante recursividad
+// Creacion de metodo moverhanoi para para resolver el juego mediante recursividad
 MoverHanoi(discos, torreA, torreC, torreB, "A", "C", "B");
 
 void MoverHanoi(int cantidad, Stack<int> origen, Stack<int> destino, Stack<int> auxiliar, string nomOrigen, string nomDestino, string nomAux)
 {
-    
+    if (cantidad == 1)
+    {
+        MoverDisco(origen, destino, nomOrigen, nomDestino);
+        return;
+    }
+
+    MoverHanoi(cantidad - 1, origen, auxiliar, destino, nomOrigen, nomAux, nomDestino);
+    MoverDisco(origen, destino, nomOrigen, nomDestino);
+    MoverHanoi(cantidad - 1, auxiliar, destino, origen, nomAux, nomDestino, nomOrigen);
+}
+// Metodo para mover el disco 
+void MoverDisco(Stack<int> desde, Stack<int> hacia, string nomDesde, string nomHacia)
+{
+    int disco = desde.Pop();
+    hacia.Push(disco);
+    Console.WriteLine("Se movió el disco " + disco + " de torre " + nomDesde + " a torre " + nomHacia);
 }
