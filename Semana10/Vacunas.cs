43a0a686ec040001598d344b7cdbde3204a3c10e
Yelena Alvarado <ys.alvaradoo@uea.edu.ec>
2025-08-23 23:46:08 -0500

    Creacion de 75 pacientes Pfizer y 75 Astrazeneca

--- Semana10/Vacunas.cs
+++ Semana10/Vacunas.cs
@@ -2,7 +2,25 @@
 {
     public static void run()
     {
-        
+        Random random = new Random();
+
+        // Generar 75 pacientes unicos para Pfizer
+        HashSet<string> pfizer = new HashSet<string>();
+        while (pfizer.Count < 75)
+        {
+            pfizer.Add("PX " + random.Next(1, 501));
+        }
+        // Se genera 75 pacientes unicos para Astrazeneca diferentes de Pfizer
+        HashSet<string> astrazeneca = new HashSet<string>();
+        while (astrazeneca.Count < 75)
+        {
+            string candidato = "PX" + random.Next(1, 501);
+            if (!pfizer.Contains(candidato))
+            {
+                astrazeneca.Add(candidato);
+            }
+        }    
+
     }
 }
     
